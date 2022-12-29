c94e7b976a66120e39428d8b2b5192538b3e2a03
Jacky Su <hxsushi@126.com>
2022-12-29 13:06:11 +0000

    set selected chart as cookie

--- src/components/ChartItem.vue
+++ src/components/ChartItem.vue
@@ -7,25 +7,25 @@
   <div class="chart_container">
     <button
       class="ui red tertiary button"
-      @click="graphNumber=0"
+      @click="setSelection(0)"
     >
       Bar Chart
     </button>
     <button
       class="ui orange tertiary button"
-      @click="graphNumber=1"
+      @click="setSelection(1)"
     >
       Pie Chart
     </button>
     <button
       class="ui yellow tertiary button"
-      @click="graphNumber=2"
+      @click="setSelection(2)"
     >
       3D Chart
     </button>
     <button
       class="ui olive tertiary button"
-      @click="graphNumber=3"
+      @click="setSelection(3)"
     >
       Line Chart
     </button>
@@ -48,6 +48,23 @@
     },
     components: {
       ChartLoaderVue
+    },
+    methods: {
+      setSelection(graphNumber) {
+        this.graphNumber = graphNumber;
+        $cookies.set("selectedGraph", graphNumber);
+      },
+      initSelection() {
+        if ($cookies.isKey("selectedGraph")) {
+          this.graphNumber = $cookies.get("selectedGraph")
+        } else {
+          this.graphNumber = 0;
+          $cookies.set("selectedGraph", 0);
+        }
+      }
+    },
+    created() {
+      this.initSelection()
     }
   }
 </script>
